21b33e7e6acb00b8987eb5f53d5790dd339fa19d
Yogesh Pawar <78149377+Yogeshpomnixm@users.noreply.github.com>
2025-05-29 10:43:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -298,9 +298,9 @@ if user_question:
                 if result_df is not None and not result_df.empty:
                     if result_df.shape == (1, 1):
                         result_value = result_df.iloc[0, 0]
-                        response = ask_SmartResponse(user_question, result_value)+{python_expr}
+                        response =python_expr #ask_SmartResponse(user_question, result_value)
                     else:
-                        response = ask_SmartResponse(user_question, result_df)+{python_expr}
+                        response =python_expr #ask_SmartResponse(user_question, result_df)
                 else:
                     # Case 1: Query ran successfully but returned no rows.
                     # This is where you want your "no data" smart answer.
